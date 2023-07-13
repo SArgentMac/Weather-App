7253a5b942c42d9339e990b7d2f2ae98f0562e6d
SArgentMac <138621306+SArgentMac@users.noreply.github.com>
2023-07-13 05:55:46 -0700

    added min temp and footer

--- index.js
+++ index.js
@@ -29,30 +29,41 @@ let formatDate = `${day} ${hours}:${minutes}`;
 let currentDate = document.querySelector("h1#date");
 currentDate.innerHTML = formatDate;
 
-function search(event) {
-    event.preventDefault();
-    let changeCity = document.querySelector("#search-input");
+function searchCity(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${changeCity.value}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
   }
+
+  function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
   function showWeather(response) {
     console.log(response.data);
-    let currentTemp = document.querySelector("#temp");
-    let temperature = Math.round(response.data.main.temp);
-    currentTemp.innerHTML = ` ${temperature}`;
-    let windCurrent = Math.round(response.data.wind.speed);
-    let windVal = document.querySelector("#wind");
-    windVal.innerHTML = windCurrent;
-    let humidity = Math.round(response.data.main.humidity);
-    let humidityVal = document.querySelector("#humidity");
-    humidityVal.innerHTML = humidity;
-    let cityName = document.querySelector("#city");
-    cityName.innerHTML = response.data.name;
+     document.querySelector("#temp").innerHTML = 
+     Math.round(response.data.main.temp);
+    
+    document.querySelector("#temp-min").innerHTML =
+     Math.round(response.data.main.temp_min);
+    
+    document.querySelector("#wind").innerHTML = 
+    Math.round(response.data.wind.speed);
+    
+   
+    document.querySelector("#humidity").innerHTML = 
+    Math.round(response.data.main.humidity);
+    
+   document.querySelector("#city").innerHTML =
+   response.data.name;
+    
+    document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
   }
   
   let citySearch = document.querySelector("#search-form");
-  citySearch.addEventListener("submit", search);
+  citySearch.addEventListener("submit", handleSubmit);
   
   function retrievePosition(position) {
     let apiKey = "80e98c1f9b189c5222b5d0b3dc2f051c";
@@ -70,6 +81,7 @@ function search(event) {
   let theLocation = document.querySelector("#current-location");
   theLocation.addEventListener("click", currentPosition);
   
+  searchCity("London");
   
   
   
