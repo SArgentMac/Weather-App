c39f5769f243c35d1a8b87bac915c199a3747dfc
SArgentMac <138621306+SArgentMac@users.noreply.github.com>
2023-07-17 21:35:47 +0100

    Added temperature unit links and icons

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let date = new Date();
+function formatDate (timestamp){
+let date = new Date(timestamp);
 
 let hours = date.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
 }
-else 
-{hours = `${hours}`;}
 
 let minutes = date.getMinutes();
 if (minutes < 10) {
@@ -24,10 +23,8 @@ let days = [
 ];
 let day = days[dayIndex];
 
-let formatDate = `${day} ${hours}:${minutes}`;
-
-let currentDate = document.querySelector("h1#date");
-currentDate.innerHTML = formatDate;
+return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
     let apiKey = "e852e002t3a2a44o7a23fa349fd1bcd4";
@@ -42,22 +39,21 @@ function searchCity(city) {
 }
   function showWeather(response) {
     console.log(response.data);
-     document.querySelector("#temp").innerHTML = 
-     Math.round(response.data.temperature.current);
+    celsiusTemperature = response.data.temperature.current;
     document.querySelector("#wind").innerHTML = 
     Math.round(response.data.wind.speed);
     let iconElement = document.querySelector("#icon");
-    console.log(response.data.condition.icon_url);
     iconElement.setAttribute("src", ` ${response.data.condition.icon_url} `);
-   
     document.querySelector("#humidity").innerHTML = 
-    Math.round(response.data.temperature.humidity);
-    
+    Math.round(response.data.temperature.humidity); 
    document.querySelector("#city").innerHTML =
-   response.data.city;
-    
+   response.data.city; 
     document.querySelector("#description").innerHTML =
     response.data.condition.description;
+    let currentDate = document.querySelector("h1#date");
+    currentDate.innerHTML = formatDate(response.data.time * 1000);
+     document.querySelector("#temp").innerHTML = Math.round(celsiusTemperature);
+    
   }
   
   let citySearch = document.querySelector("#search-form");
@@ -76,15 +72,36 @@ function searchCity(city) {
     navigator.geolocation.getCurrentPosition(retrievePosition);
   }
   
-  let theLocation = document.querySelector("#current-location");
+
+  function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temp");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+  
+  function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temp");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+  
+  let celsiusTemperature = null;
+
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+  
+let theLocation = document.querySelector("#current-location");
   theLocation.addEventListener("click", currentPosition);
   
   searchCity("London");
-  
-  
-  //let iconElement = document.querySelector("#icon");
-  //console.log(response.data.condition.icon_url);
- //iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
   
   
   
